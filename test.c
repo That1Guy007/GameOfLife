e22dca40af9941d4463e000dfa07eb513f0a102c
that1guy007 <cjh3rnand3z@gmail.com>
2017-11-13 16:00:07 -0500

    Working life, edit wrap

--- test.c
+++ test.c
@@ -1,35 +1,21 @@
 #include <mpi.h>
-//#define MPE_GRAPHICS
-//#include "mpe.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-//static MPE_XGraph graph;
-
-//static char *displayname = 0;
-//extern void   srand48();
-//extern double drand48();
-//extern char * malloc();
-//static int width = 400, height = 400;
 
 #define BORN 1
 #define DIES 0
 
 /* The Life function */
-double life(matrix_size, ntimes, comm)
-int matrix_size;
-int ntimes;
+double life(int matrix_size, int ntimes, MPI_Comm comm){
 
-MPI_Comm comm;
-{
-  int      rank, size ;
+  int      rank, size ; //rank is the node which is working on the program, the size is the total nodes in the system
   int      next, prev ;
   int      i, j, k;
   int      mysize, sum ;
   int    **matrix, **temp, **addr ;
   double   slavetime, totaltime, starttime ;
-  int      my_offset;
+//  int      my_offset;
 
   /* Determine size and my rank in communicator */
   MPI_Comm_size(comm, &size) ;
@@ -47,50 +33,49 @@ MPI_Comm comm;
 
 
   /* Determine my part of the matrix */
-  mysize = matrix_size/size + ((rank < (matrix_size % size)) ? 1 : 0 ) ;
-  my_offset = rank * (matrix_size/size);
-  if (rank > (matrix_size % size)) my_offset += (matrix_size % size);
-  else                             my_offset += rank;
+  mysize = matrix_size/size ; //+ ((rank < (matrix_size % size)) ? 1 : 0 ) ;
+  /*
+   * rows
+   */
+  /*my_offset = rank * (matrix_size/size);
+  if (rank > (matrix_size % size))
+	  my_offset += (matrix_size % size);
+  else
+	  my_offset += rank;*/
 
   /* allocate the memory dynamically for the matrix */
   matrix = (int **)malloc(sizeof(int *)*(mysize+2)) ;
   temp = (int **)malloc(sizeof(int *)*(mysize+2)) ;
+
   for (i = 0; i < mysize+2; i++) {
     matrix[i] = (int *)malloc(sizeof(int)*(matrix_size+2)) ;
-    temp[i] = (int *)malloc(sizeof(int)*(matrix_size+2)) ;
+    temp[i] = (int *)malloc(sizeof(int)*(matrix_size+2))  ; //columns
   }
 
-  /* Initialize the boundaries of the life matrix */
+  /* Initialize the boundaries of the life matrix *//*
   for (j = 0; j < matrix_size+2; j++)
     matrix[0][j] = matrix[mysize+1][j] = temp[0][j] = temp[mysize+1][j] = DIES ;
   for (i = 0; i < mysize+2; i++)
-    matrix[i][0] = matrix[i][matrix_size+1] = temp[i][0] = temp[i][matrix_size+1] = DIES ;
+    matrix[i][0] = matrix[i][matrix_size+1] = temp[i][0] = temp[i][matrix_size+1] = DIES ;*/
 
   /* Initialize the life matrix */
+
   srand(time(NULL));
 
-  for (i = 1; i <= mysize; i++)  {
-    //srand(time(NULL));
-    for (j = 1; j<= matrix_size; j++){
-
+  for (i = 0; i < mysize; i++)  { //rows
+    for (j = 0; j< matrix_size; j++){ //collumns
     	if ((rand() % 2) == 1)
     		matrix[i][j] = BORN ;
       else
         matrix[i][j] = DIES ;
-
-	//printf("%d ", matrix[i][j]);
     }
-    //printf("\n");
   }
-//printf("\n");
 
-  /* Open the graphics display */
-  /*MPE_Open_graphics( &graph, MPI_COMM_WORLD, displayname,
-		   -1, -1, width, height, 0 );
-*/
   /* Play the game of life for given number of iterations */
   starttime = MPI_Wtime() ;
+
   for (k = 0; k < ntimes; k++) {
+
     MPI_Request      req[4];
     MPI_Status       status[4];
 
@@ -102,10 +87,13 @@ MPI_Comm comm;
     MPI_Waitall(4, req, status);
 
     /* For each element of the matrix ... */
-    for (i = 1; i <= mysize; i++) {
-      for (j = 1; j < matrix_size+1; j++) {
+    for (i = 0; i < mysize; i++) {
+      for (j = 0; j < matrix_size; j++) {
 
         /* find out the value of the current cell */
+
+    	  //need to wrap here!!!!!!!!
+
         sum = matrix[i-1][j-1] + matrix[i-1][j] + matrix[i-1][j+1]
           + matrix[i][j-1] + matrix[i][j+1]
             + matrix[i+1][j-1] + matrix[i+1][j] + matrix[i+1][j+1] ;
@@ -118,32 +106,17 @@ MPI_Comm comm;
         else
           temp[i][j] = matrix[i][j] ;
 		printf("%d ", matrix[i][j]);
-
-	/*{ int xloc, yloc, xwid, ywid;
-	  xloc = ((my_offset + i - 1) * width) / matrix_size;
-	  yloc = ((j - 1) * height) / matrix_size;
-	  xwid = ((my_offset + i) * width) / matrix_size - xloc;
-	  ywid = (j * height) / matrix_size - yloc;
-	  MPE_Fill_rectangle( graph, xloc, yloc, xwid, ywid, temp[i][j] );
-	  }*/
       }
       printf("\n");
     }
     printf("\n");
-    //MPE_Update( graph );
+
     /* Swap the matrices */
     addr = matrix ;
     matrix = temp ;
     temp = addr ;
   }
-  /*
-for (i = 1; i <= matrix_size; i++) {
-		for (j = 1; j < matrix_size + 1; j++) {
 
-			printf("%d ", matrix[i][j]);
-		}
-		printf("\n");
-	}*/
   /* Return the average time taken/processor */
   slavetime = MPI_Wtime() - starttime;
   MPI_Reduce (&slavetime, &totaltime, 1, MPI_DOUBLE, MPI_SUM, 0, comm);
@@ -169,27 +142,17 @@ char *argv[] ;
      that does not end in a newline.  MPI does not require this, though
      high-quality implementations will do this.
    */
-//#if !defined (SGI_MPI) && !defined (IBM_MPI)
   if ( rank == 0 ) {
     printf("Matrix Size : ") ;
     scanf("%d",&N) ;
     printf("Iterations : ") ;
     scanf("%d",&iters) ;
   }
-//#else
- // N=20;
- // iters=50;
-//#endif
 
   /* Broadcast the size and # of iterations to all processes */
   MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD) ;
   MPI_Bcast(&iters, 1, MPI_INT, 0, MPI_COMM_WORLD) ;
 
-  /*if (argc > 2 && strcmp( argv[1], "-display" ) == 0) {
-      displayname = malloc( strlen( argv[2] ) + 1 );
-      strcpy( displayname, argv[2] );
-      }
-      */
   /* Call the life routine */
   time = life ( N, iters, MPI_COMM_WORLD );
 
