384160b1c8b26bc41929bf9e6b6139e6a1eddc9e
that1guy007 <cjh3rnand3z@gmail.com>
2017-11-14 23:23:15 -0500

    Almost done, rank issues solved, sum issues

--- test.c
+++ test.c
@@ -1,3 +1,7 @@
+//http://beige.ucs.indiana.edu/I590/node115.html
+
+
+
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,11 +33,13 @@ double life(int matrix_size, int ntimes, MPI_Comm comm) {
 	 */
 
 	if (rank == 0)
-		prev = MPI_PROC_NULL; // means there is no previous process
+		prev = size -1;
+		//prev = MPI_PROC_NULL; // means there is no previous process
 	else
 		prev = rank - 1;
 	if (rank == size - 1)
-		next = MPI_PROC_NULL; // means there is no next process
+		next = 0;
+		//next = MPI_PROC_NULL; // means there is no next process
 	else
 		next = rank + 1;
 
@@ -100,10 +106,10 @@ double life(int matrix_size, int ntimes, MPI_Comm comm) {
 
 	for (k = 0; k < ntimes; k++) {
 
-		// MPI_Request      req[4];
-		//MPI_Status       status[4];
+		MPI_Request      req[4];
+		MPI_Status       status[4];
 
-		/* Send and receive boundary information
+		 /*Send and receive boundary information
 		 MPI_Isend(&matrix[1][0],matrix_size+2,MPI_INT,prev,0,comm,req);
 		 MPI_Irecv(&matrix[0][0],matrix_size+2,MPI_INT,prev,0,comm,req+1);
 		 MPI_Isend(&matrix[mysize][0],matrix_size+2,MPI_INT,next,0,comm,req+2);
@@ -116,23 +122,41 @@ double life(int matrix_size, int ntimes, MPI_Comm comm) {
 		 * THIS ISNT RIGHT YET....NEED TO MODIFY COE ABOVE (NEXT AND PREVIOUS)
 		 */
 		for (int loop = 0; loop < matrix_size; loop++) {
+			 /*Send and receive boundary information*/
+					 MPI_Isend(&matrix[0][loop],1,MPI_INT,prev,0,comm,req);
+					 MPI_Irecv(&addr[0][loop],1,MPI_INT,prev,0,comm,req+1);
+					 MPI_Isend(&matrix[matrix_size -1][loop],1,MPI_INT,next,0,comm,req+2);
+					 MPI_Irecv(&matrix[1][loop],1,MPI_INT,next,0,comm,req+3);
+					 MPI_Waitall(4, req, status);
+
+/*
 			//MPI_Send(&matrix[loop][0], 1, MPI_INT, rank -1, 0, MPI_COMM_WORLD);//left side
-			MPI_Send(&matrix[0][loop], 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD); //top of array to array above
+			MPI_Send(&matrix[0][loop], 1, MPI_INT, prev, 0, MPI_COMM_WORLD); //top of array to array above
+
+			MPI_Recv(&addr[0][loop], 1, MPI_INT, next, 0, MPI_COMM_WORLD,
+								MPI_STATUS_IGNORE);    //bottom of the array before it. IE the shared top
+
 			//MPI_Send(&matrix[0][loop], 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);//top of array to array below
 			//MPI_Send(&matrix[matrix_size -1][loop], 1, MPI_INT, rank -1, 0, MPI_COMM_WORLD);//bottom
-			MPI_Send(&matrix[matrix_size - 1][loop], 1, MPI_INT, rank + 1, 0,
+			MPI_Send(&matrix[matrix_size - 1][loop], 1, MPI_INT, next, 0,
 					MPI_COMM_WORLD);    //bottom
 			//MPI_Send(&matrix[loop][matrix_size -1], 1, MPI_INT, rank -1, 0, MPI_COMM_WORLD);//right
 
-			MPI_Recv(&addr[0][loop], 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD,
-					MPI_STATUS_IGNORE);    //bottom of the array before it. IE the shared top
-			MPI_Recv(&addr[1][loop], 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD,
+
+			MPI_Recv(&addr[1][loop], 1, MPI_INT, prev, 0, MPI_COMM_WORLD,
 					MPI_STATUS_IGNORE);    //top of the array after it IE the bottom shared
 
 			//MPI_Recv(&addr[2][loop], 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			//MPI_Recv(&addr[3][loop], 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			//MPI_Recv(&addr[3][loop], 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);*/
 
 		}
+
+		for (int loop = 0; loop < matrix_size; loop++){
+			printf("%d ",addr[0][loop]);
+		}
+		printf("\n");
+
+
 		/* For each element of the matrix ... */
 		for (i = 0; i < mysize; i++) {
 			for (j = 0; j < matrix_size; j++) {
@@ -227,12 +251,13 @@ double life(int matrix_size, int ntimes, MPI_Comm comm) {
 
 				/* check if the cell dies or life is born */
 				if (sum < 2 || sum > 3)
-					temp[i][j] = DIES;
+					matrix[i][j] = DIES;
 				else if (sum == 3)
-					temp[i][j] = BORN;
-				else
-					temp[i][j] = matrix[i][j];
-				printf("%d ", matrix[i][j]);
+					matrix[i][j] = BORN;
+				//else
+				//	matrix[i][j] = matrix[i][j];
+				printf("%d|%d ", matrix[i][j], sum);
+				sum = 0;
 			}
 			printf("\n");
 		}
